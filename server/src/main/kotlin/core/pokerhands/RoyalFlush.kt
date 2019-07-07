0c5a4794f765e9d47edddf0d97959a30d23c8a05
Patryk Wójtowicz <patryk.zbigniew.wojtowicz@gmail.com>
2019-07-07 10:05:46 +0200

    Fix bug in RoyalFlush

--- server/src/main/kotlin/core/pokerhands/RoyalFlush.kt
+++ server/src/main/kotlin/core/pokerhands/RoyalFlush.kt
@@ -7,5 +7,5 @@ class RoyalFlush(cards: List<Card>) : PokerHand(HandRank.ROYAL_FLUSH, cards) {
 
     override fun isValidHand(): Boolean = isRoyalFlush(cards)
 
-    override fun compareWithinRank(other: PokerHand): Int = 1
+    override fun compareWithinRank(other: PokerHand): Int = 0
 }
