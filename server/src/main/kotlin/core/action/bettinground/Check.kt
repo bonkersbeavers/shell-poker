ed3d6a933f36e401d475c04bdfa77362b4b4e14d
Patryk Wójtowicz <patryk.zbigniew.wojtowicz@gmail.com>
2019-07-23 11:00:47 +0200

    Refactor accroding to new HandState changes

--- server/src/main/kotlin/core/action/bettinground/Check.kt
+++ server/src/main/kotlin/core/action/bettinground/Check.kt
@@ -1,21 +1,21 @@
 package core.action.bettinground
 
 import core.gameflow.HandState
-import core.gameflow.findNextPlayer
 
 class Check : BettingAction() {
 
+    //TODO: BUGGED FOR NOW!!!!
     override fun apply(handState: HandState): HandState {
         if (handState.activePlayer!!.position == handState.buttonPosition) {
             return handState.copy(activePlayer = null)
         }
 
-        val activePlayers = handState.playersInGame()
-        val nextPlayer = findNextPlayer(activePlayers, handState.activePlayer.position)
+        val nextPlayer = handState.findNextDecisivePlayer(handState.activePlayer.position)
 
         return handState.copy(activePlayer = nextPlayer)
     }
 
+    //TODO: BUGGED FOR NOW!!!!
     override fun innerIsLegal(handState: HandState): Boolean {
         return when {
             (handState.lastAggressor == null) -> true
