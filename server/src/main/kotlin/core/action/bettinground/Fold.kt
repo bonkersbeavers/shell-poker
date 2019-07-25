552cce6bac6fa18fbd37ed5fb1d9dafcdd04ff28
Mateusz Szpyrka <mjsz1701@gmail.com>
2019-07-26 01:16:26 +0200

    Fix Fold class due to changes in BettingAction

--- server/src/main/kotlin/core/action/bettinground/Fold.kt
+++ server/src/main/kotlin/core/action/bettinground/Fold.kt
@@ -6,16 +6,7 @@ class Fold : BettingAction() {
 
     override fun innerApply(handState: HandState): HandState {
         val updatedPlayer = handState.activePlayer!!.afterFold()
-        val updatedState = handState.updateActivePlayer(updatedPlayer)
-
-        val nextPlayer = updatedState.nextDecisivePlayer(updatedPlayer)
-
-        return when {
-            (updatedState.decisivePlayers.size == 1) or (nextPlayer == handState.lastAggressor) ->
-                updatedState.copy(activePlayer = null)
-
-            else -> updatedState.copy(activePlayer = nextPlayer)
-        }
+        return handState.updateActivePlayer(updatedPlayer)
     }
 
     override fun innerIsLegal(handState: HandState): Boolean = true
