e272f8296db2ea76c84c1b5223a66fbb2992c0e1
Mateusz Szpyrka <mjsz1701@gmail.com>
2019-07-26 00:24:40 +0200

    Add bettingRound and extraBet fields to HandState

--- server/src/main/kotlin/core/gameflow/HandState.kt
+++ server/src/main/kotlin/core/gameflow/HandState.kt
@@ -3,13 +3,14 @@ package core.gameflow
 import core.Card
 
 /**
- * activePlayer set to null indicates that there is no further action possible in given betting round
+ * activePlayer set to null indicates that there is no further action possible in given betting round.
  *
  * lastAggressor parameter should be used to determine when the action in a betting round should end,
- * as well as to resolve the order in which players show their cards during the showdown. That's why
- * every time a betting rounds starts with a check, the first player that checks should be marked
- * as the last aggressor (even though no raise was made). The same rule applies when first player
- * calls the big blind in a pre-flop betting round.
+ * as well as to resolve the order in which players show their cards during the showdown. If set to null,
+ * it indicates that no aggressive action was taken yet.
+ *
+ * extraBet is used when there is an incomplete raise all-in. It is used to store the additional bet value.
+ * It should be reset to 0 every time a legal bet / raise is made.
  */
 data class HandState(
     val players: List<Player>,
@@ -17,11 +18,14 @@ data class HandState(
     val buttonPosition: Int,
     val activePlayer: Player?,
     val communityCards: List<Card> = emptyList(),
+    val bettingRound: BettingRound = BettingRound.PRE_FLOP,
     val lastAggressor: Player? = null,
     val currentBet: Int = 0,
-    val minRaise: Int = blinds.bigBlind
+    val minRaise: Int = blinds.bigBlind,
+    val extraBet: Int = 0
 ) {
     init {
+        assert(players.size >= 2)
         assert(players.distinctBy { it.position }.size == players.size)
 
         if (activePlayer != null)
@@ -32,6 +36,21 @@ data class HandState(
     }
 
     val pot: Int = players.sumBy { it.chipsInPot }
+    val totalBet: Int = currentBet + extraBet
+
+    val playersInGame: List<Player> = players.filter { it.isInGame() }
+    val decisivePlayers: List<Player> = players.filter { it.isDecisive() }
+
+    val smallBlindPlayer: Player
+        get() {
+            val firstPlayer = nextPlayer(buttonPosition)
+            return if (players.size >= 3)
+                firstPlayer
+            else
+                nextPlayer(firstPlayer)
+        }
+
+    val bigBlindPlayer: Player = nextPlayer(smallBlindPlayer)
 
     fun updateActivePlayer(playerUpdate: Player): HandState {
 
@@ -54,13 +73,11 @@ data class HandState(
         return begin + end
     }
 
-    val playersInGame: List<Player> = players.filter { it.isInGame() }
-
-    val decisivePlayers: List<Player> = players.filter { it.isDecisive() }
-
     fun nextPlayer(position: Int): Player = orderedPlayers(position + 1).first()
+    fun nextPlayer(player: Player): Player = nextPlayer(player.position)
 
     fun nextDecisivePlayer(position: Int): Player? = orderedPlayers(position + 1).find { it.isDecisive() }
+    fun nextDecisivePlayer(player: Player): Player? = nextDecisivePlayer(player.position)
 
     fun prevPlayer(position: Int): Player = orderedPlayers(position).last()
 
