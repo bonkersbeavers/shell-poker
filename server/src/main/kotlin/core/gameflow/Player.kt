cc4bf419fa835c4e8e045dddeeec615960a13bdb
Mateusz Szpyrka <mjsz1701@gmail.com>
2019-07-28 16:02:55 +0200

    Change lastAction type from BettingAction to ActionType, add helper maxBet and withBet members

--- server/src/main/kotlin/core/gameflow/Player.kt
+++ server/src/main/kotlin/core/gameflow/Player.kt
@@ -4,38 +4,36 @@ import core.Card
 import core.action.bettinground.*
 
 /**
- * handStartingStack - player's stack size at the beginning of currently played hand,
- *      it remains unchanged throughout the hand
- *
- * chipsInPot - the amount of player's chips that already went into the pot,
+ * chipsInPot - the amount of player's chips that already went into the pot in previous rbetting rounds,
  *      not including the chips in player's current bet
  */
 data class Player(
     val position: Int,
-    private val handStartingStack: Int,
+    val stack: Int,
     val holeCards: List<Card> = emptyList(),
+    val bet: Int = 0,
     val chipsInPot: Int = 0,
-    val lastAction: BettingAction? = null
+    val lastAction: ActionType? = null
 ) {
-
-    /* the amount od chips available for the player at the beginning of current betting round */
-    private val roundStartingStack: Int = handStartingStack - chipsInPot
-
-    val currentBet: Int = when (lastAction) {
-        is Post -> lastAction.size
-        is Call -> lastAction.size
-        is Bet -> lastAction.size
-        is Raise -> lastAction.size
-        is AllIn -> roundStartingStack
-        else -> 0   // no action / check / fold
+    init {
+        assert(stack >= 0)
+        assert(bet >= 0)
+        assert(chipsInPot >= 0)
     }
 
-    /* the amount of chips that haven't been neither put into the pot nor the current bet */
-    val stack: Int = roundStartingStack - currentBet
+    val isInGame: Boolean = lastAction != ActionType.FOLD
+    val isAllIn: Boolean = stack == 0
+    val isDecisive: Boolean = isInGame and !isAllIn
+
+    val maxBet: Int = bet + stack
+
+    fun withBet(betSize: Int): Player {
+        val difference = betSize - bet
+        return this.copy(
+                stack = stack - difference,
+                bet = betSize
+        )
+    }
 
     fun withCards(newCards: List<Card>): Player = this.copy(holeCards = newCards)
-
-    val isInGame: Boolean = lastAction !is Fold
-    val isAllIn: Boolean = lastAction is AllIn
-    val isDecisive: Boolean = isInGame and !isAllIn
 }
