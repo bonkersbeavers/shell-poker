d75387651f33ba633badb6e990b0ca22e8167d06
Mateusz Szpyrka <mjsz1701@gmail.com>
2019-07-29 00:41:50 +0200

    Add Check action implementation

--- server/src/main/kotlin/core/action/bettinground/Check.kt
+++ server/src/main/kotlin/core/action/bettinground/Check.kt
@@ -6,8 +6,7 @@ class Check : BettingAction(ActionType.CHECK) {
 
     override fun innerApply(handState: HandState): HandState {
 
-        // TODO: when BB / straddler checks, the info about posted blind is lost
-        val updatedPlayer = handState.activePlayer!!.copy(lastAction = this)
+        val updatedPlayer = handState.activePlayer!!.copy(lastAction = ActionType.CHECK)
         return handState.updateActivePlayer(updatedPlayer)
     }
 
@@ -18,7 +17,7 @@ class Check : BettingAction(ActionType.CHECK) {
             (handState.totalBet == 0) -> true
 
             /* Pre flop scenario in which BB / straddler should have an option to check */
-            ((activePlayer.currentBet == handState.totalBet) and (activePlayer.lastAction is Post)) -> true
+            ((activePlayer.bet == handState.totalBet) and (activePlayer.lastAction == ActionType.POST)) -> true
 
             else -> false
         }
