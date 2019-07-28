df274def4ef957bd17d91ff8f87e7e7d70bcc272
Mateusz Szpyrka <mjsz1701@gmail.com>
2019-07-29 00:41:59 +0200

    Add Post action implementation

--- server/src/main/kotlin/core/action/bettinground/Post.kt
+++ server/src/main/kotlin/core/action/bettinground/Post.kt
@@ -1,12 +1,39 @@
 package core.action.bettinground
 
 import core.gameflow.HandState
+import kotlin.math.min
 
 class Post(val size: Int) : BettingAction(ActionType.POST) {
 
-    // TODO
-    override fun innerApply(handState: HandState): HandState = handState
+    override fun innerApply(handState: HandState): HandState {
+        val activePlayer = handState.activePlayer!!
+        val bet = min(size, activePlayer.maxBet)
+        val playerUpdate = activePlayer
+                .withBet(bet)
+                .copy(lastAction = ActionType.POST)
 
-    // TODO
+        val newState = handState.updateActivePlayer(playerUpdate)
+
+        return when {
+
+            /* When posted bet is lower than current bet in the hand. */
+            bet <= newState.totalBet -> newState
+
+            /* When posted bet has an effect of raising current bet in the hand. */
+            bet >= newState.minRaise -> {
+                val difference = bet - newState.totalBet
+                val newMinRaise = bet + difference
+                newState.copy(lastLegalBet = bet, extraBet = 0, minRaise = newMinRaise)
+            }
+
+            /* When posted bet is higher than current bet in the hand but not high enough
+             * to be considered a legal raise*/
+            else -> {
+                val extra = bet - newState.lastLegalBet
+                newState.copy(extraBet = extra)
+            }
+        }
+    }
+
     override fun innerIsLegal(handState: HandState): Boolean = true
 }
