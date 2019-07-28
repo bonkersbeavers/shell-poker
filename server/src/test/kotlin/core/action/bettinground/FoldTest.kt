4e74996577d80d7e8a083e568ed8a20a43852c29
Mateusz Szpyrka <mjsz1701@gmail.com>
2019-07-29 00:42:59 +0200

    Fix Fold test after Player and HandState classes refactor

--- server/src/test/kotlin/core/action/bettinground/FoldTest.kt
+++ server/src/test/kotlin/core/action/bettinground/FoldTest.kt
@@ -14,61 +14,64 @@ class FoldTest {
 
     @Test
     fun `folding with active players ahead should simply move activePlayer`() {
-        val player0 = Player(position = 0, stack = 150, currentBet = 100) // original raiser
-        val player1 = Player(position = 1, stack = 200) // active player
-        val player2 = Player(position = 2, stack = 100, folded = true)
-        val player3 = Player(position = 3, stack = 100, currentBet = 50)
+        val player0 = Player(position = 0, stack = 100, bet = 100, lastAction = ActionType.RAISE) // BTN / aggressor
+        val player1 = Player(position = 1, stack = 200, bet = 0, lastAction = ActionType.CHECK) // active player
+        val player2 = Player(position = 2, stack = 100, bet = 50, lastAction = ActionType.BET)
+        val player3 = Player(position = 3, stack = 100, bet = 0, lastAction = ActionType.FOLD)
 
         val state = HandState(
                 players = listOf(player0, player1, player2, player3),
                 blinds = blindsMock,
                 buttonPosition = 0,
                 activePlayer = player1,
-                currentBet = 100
+                lastLegalBet = 100,
+                bettingRound = BettingRound.FLOP
         )
 
         val fold = Fold()
         val newState = fold.apply(state)
 
-        assert(newState.activePlayer == player3)
-        assert(newState.playersInGame.toSet() == setOf(player0, player3))
+        assert(newState.activePlayer == player2)
+        assert(newState.playersInGame.toSet() == setOf(player0, player2))
     }
 
     @Test
     fun `folding to original raiser should set activePlayer to null`() {
-        val player0 = Player(position = 0, stack = 0, folded = true)
-        val player1 = Player(position = 1, stack = 100, currentBet = 100) // original raiser
-        val player2 = Player(position = 2, stack = 100, currentBet = 100)
-        val player3 = Player(position = 3, stack = 200) // active player
+        val player0 = Player(position = 0, stack = 150, bet = 100, lastAction = ActionType.BET) // BTN / aggressor
+        val player1 = Player(position = 1, stack = 200, bet = 0, lastAction = ActionType.FOLD)
+        val player2 = Player(position = 2, stack = 100, bet = 100, lastAction = ActionType.CALL)
+        val player3 = Player(position = 3, stack = 100, bet = 0, lastAction = ActionType.CHECK) // active player
 
         val state = HandState(
                 players = listOf(player0, player1, player2, player3),
                 blinds = blindsMock,
                 buttonPosition = 0,
                 activePlayer = player3,
-                currentBet = 100
+                lastLegalBet = 100,
+                bettingRound = BettingRound.FLOP
         )
 
         val fold = Fold()
         val newState = fold.apply(state)
 
         assert(newState.activePlayer == null)
-        assert(newState.playersInGame.toSet() == setOf(player1, player2))
+        assert(newState.playersInGame.toSet() == setOf(player0, player2))
     }
 
     @Test
     fun `folding with only big blind left in game (no aggressor) should set activePlayer to null`() {
-        val player0 = Player(position = 0, stack = 100, folded = true)
-        val player1 = Player(position = 1, stack = 100, folded = true)
-        val player2 = Player(position = 2, stack = 100, currentBet = 50) // active player / small blind
-        val player3 = Player(position = 3, stack = 100, currentBet = 100) // big blind
+        val player0 = Player(position = 0, stack = 100, bet = 0, lastAction = ActionType.FOLD)
+        val player1 = Player(position = 1, stack = 100, bet = 0, lastAction = ActionType.FOLD) // BTN
+        val player2 = Player(position = 2, stack = 100, bet = 50, lastAction = ActionType.POST) // SB / active player
+        val player3 = Player(position = 3, stack = 100, bet = 100, lastAction = ActionType.POST) // BB
 
         val state = HandState(
                 players = listOf(player0, player1, player2, player3),
                 blinds = blindsMock,
-                buttonPosition = 0,
+                buttonPosition = 1,
                 activePlayer = player2,
-                currentBet = 100
+                lastLegalBet = 100,
+                bettingRound = BettingRound.PRE_FLOP
         )
 
         val fold = Fold()
@@ -79,18 +82,18 @@ class FoldTest {
     }
 
     @Test
-    fun `folding to big blind pre-flop should set activePlayer to BB if no raise was made`() {
-        val player0 = Player(position = 0, stack = 100, folded = true)
-        val player1 = Player(position = 1, stack = 100, currentBet = 100) // button
-        val player2 = Player(position = 2, stack = 100, currentBet = 50) // active player / small blind
-        val player3 = Player(position = 3, stack = 100, currentBet = 100) // big blind
+    fun `folding to big blind pre-flop should set activePlayer to BB even if no raise was made`() {
+        val player0 = Player(position = 0, stack = 100, bet = 0, lastAction = ActionType.FOLD)
+        val player1 = Player(position = 1, stack = 100, bet = 100, lastAction = ActionType.CALL) // BTN
+        val player2 = Player(position = 2, stack = 100, bet = 50, lastAction = ActionType.POST) // SB / active player
+        val player3 = Player(position = 3, stack = 100, bet = 100, lastAction = ActionType.POST) // BB
 
         val state = HandState(
                 players = listOf(player0, player1, player2, player3),
                 blinds = Blinds(50, 100),
                 buttonPosition = 1,
                 activePlayer = player2,
-                currentBet = 100,
+                lastLegalBet = 100,
                 bettingRound = BettingRound.PRE_FLOP
         )
 
@@ -102,18 +105,18 @@ class FoldTest {
     }
 
     @Test
-    fun `folding to big blind pre-flop should end the action if a raise was made`() {
-        val player0 = Player(position = 0, stack = 100, folded = true)
-        val player1 = Player(position = 1, stack = 100, currentBet = 200) // button
-        val player2 = Player(position = 2, stack = 100, currentBet = 50) // active player / small blind
-        val player3 = Player(position = 3, stack = 100, currentBet = 200) // big blind (raiser)
+    fun `folding to big blind pre-flop should end the action if BB raised before`() {
+        val player0 = Player(position = 0, stack = 100, bet = 0, lastAction = ActionType.FOLD)
+        val player1 = Player(position = 1, stack = 100, bet = 200, lastAction = ActionType.CALL) // BTN
+        val player2 = Player(position = 2, stack = 100, bet = 100, lastAction = ActionType.CALL) // SB / active player
+        val player3 = Player(position = 3, stack = 100, bet = 200, lastAction = ActionType.RAISE) // BB / aggressor
 
         val state = HandState(
                 players = listOf(player0, player1, player2, player3),
                 blinds = Blinds(50, 100),
                 buttonPosition = 1,
                 activePlayer = player2,
-                currentBet = 200,
+                lastLegalBet = 200,
                 bettingRound = BettingRound.PRE_FLOP
         )
 
