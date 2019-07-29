66fcee855a1a1f3a93656d217f67d58e4ecc5a25
Mateusz Szpyrka <mjsz1701@gmail.com>
2019-07-29 22:10:21 +0200

    Add Raise action implementation

--- server/src/main/kotlin/core/action/bettinground/Raise.kt
+++ server/src/main/kotlin/core/action/bettinground/Raise.kt
@@ -4,9 +4,36 @@ import core.gameflow.HandState
 
 class Raise(val size: Int) : BettingAction(ActionType.RAISE) {
 
-    // TODO
-    override fun innerApply(handState: HandState): HandState = handState
+    override fun innerApply(handState: HandState): HandState {
 
-    // TODO
-    override fun innerValidate(handState: HandState): ActionValidation = ValidAction()
+        val updatedPlayer = handState.activePlayer!!
+                .withBet(size)
+                .copy(lastAction = ActionType.RAISE)
+
+        val betDifference = size - handState.lastLegalBet
+
+        return handState
+                .updateActivePlayer(updatedPlayer)
+                .copy(lastLegalBet = size,
+                        extraBet = 0,
+                        minRaise = size + betDifference,
+                        lastAggressor = updatedPlayer)
+    }
+
+    override fun innerValidate(handState: HandState): ActionValidation {
+        val player = handState.activePlayer!!
+
+        return when {
+            handState.lastLegalBet == 0 ->
+                InvalidAction("Cannot raise when no bet has been made")
+
+            size > player.maxBet ->
+                InvalidAction("Raise of size $size higher than player's maximum possible bet ${player.maxBet}")
+
+            size < handState.minRaise ->
+                InvalidAction("Raise of size $size smaller than minimum legal raise ${handState.minRaise}")
+
+            else -> ValidAction()
+        }
+    }
 }
