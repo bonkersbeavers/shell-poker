dd5bbaadffc8695139ae3748f671496480c01aa1
Mateusz Szpyrka <mjsz1701@gmail.com>
2019-07-29 22:10:39 +0200

    Add AllIn action implementation

--- server/src/main/kotlin/core/action/bettinground/AllIn.kt
+++ server/src/main/kotlin/core/action/bettinground/AllIn.kt
@@ -4,8 +4,38 @@ import core.gameflow.HandState
 
 class AllIn : BettingAction(ActionType.ALL_IN) {
 
-    // TODO
-    override fun innerApply(handState: HandState): HandState = handState
+    override fun innerApply(handState: HandState): HandState {
+        val player = handState.activePlayer!!
+        val betSize = player.maxBet
+
+        val updatedPlayer = player
+                .withBet(betSize)
+                .copy(lastAction = ActionType.ALL_IN)
+
+        val newState = handState.updateActivePlayer(updatedPlayer)
+
+        return when {
+            /* All in lower or equal to previous highest bet. */
+            betSize <= handState.totalBet -> newState
+
+            /* All in higher than previous highest bet, but lower than min raise. */
+            betSize < handState.minRaise -> {
+                val betDifference = betSize - handState.lastLegalBet
+                newState.copy(extraBet = betDifference)
+            }
+
+            /* All in with effect of a regular raise. */
+            else -> {
+                val betDifference = betSize - handState.lastLegalBet
+                newState.copy(
+                        lastLegalBet = betSize,
+                        extraBet = 0,
+                        minRaise = betSize + betDifference,
+                        lastAggressor = updatedPlayer
+                )
+            }
+        }
+    }
 
     override fun innerValidate(handState: HandState): ActionValidation = ValidAction()
 }
