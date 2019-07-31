4a282fa8e9c375854b3ba34d9fe2430007e47c46
Patryk Wójtowicz <patryk.zbigniew.wojtowicz@gmail.com>
2019-07-31 13:54:11 +0200

    Initial implementation of dealHoleCards method

--- server/src/main/kotlin/core/gameflow/Dealer.kt
+++ server/src/main/kotlin/core/gameflow/Dealer.kt
@@ -1,4 +1,20 @@
 package core.gameflow
 
-class Dealer {
+import core.cards.Card
+
+class Dealer(deck: List<Card>){
+    private val deckIterator = deck.listIterator()
+
+    fun dealHoleCards(handState: HandState): HandState {
+        val oldPlayers = handState.players
+        val newPlayers: MutableList<Player> = mutableListOf()
+
+        for (player: Player in oldPlayers) {
+            newPlayers.add(
+                    player.withCards(listOf(deckIterator.next(), deckIterator.next()))
+            )
+        }
+
+        return handState.copy(players = newPlayers.toList())
+    }
 }
