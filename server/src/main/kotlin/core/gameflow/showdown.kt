eb42389844e016ef056591a1429d0a7bca5af291
Mateusz Szpyrka <mjsz1701@gmail.com>
2019-08-24 20:57:43 +0200

    Implement showdown resolving method

--- server/src/main/kotlin/core/gameflow/showdown.kt
+++ server/src/main/kotlin/core/gameflow/showdown.kt
@@ -4,3 +4,38 @@ sealed class ShowdownAction
 
 data class ShowCards(val playerId: Int) : ShowdownAction()
 data class MuckCards(val playerId: Int) : ShowdownAction()
+
+fun resolveShowdown(handState: HandState): List<ShowdownAction> {
+
+    val orderedPlayers = when {
+        handState.lastAggressor == null -> handState.orderedPlayers(handState.buttonPosition + 1)
+        else -> handState.orderedPlayers(handState.lastAggressor.position)
+    }.filter { it.isInGame }
+
+    // all players must show their cards if there are more streets to be dealt
+    if (handState.bettingRound != BettingRound.RIVER)
+        return orderedPlayers.map { ShowCards(it.id) }
+
+    // one player definitely wins with another if he has a better hand
+    // and he has put at least the same amount of chips into the pot
+    fun definitelyWins(player: Player, other: Player): Boolean {
+        val communityCards = handState.communityCards
+        return (player.hand(communityCards) > other.hand(communityCards)) and (player.chipsInPot >= other.chipsInPot)
+    }
+
+    val showingPlayers = mutableListOf<Player>()
+
+    orderedPlayers.forEach { player ->
+
+        // player should show the cards if no other player definitely wins with him
+        if (showingPlayers.none { definitelyWins(it, player) })
+            showingPlayers.add(player)
+    }
+
+    return orderedPlayers.map {
+        if (it in showingPlayers)
+            ShowCards(it.id)
+        else
+            MuckCards(it.id)
+    }
+}
