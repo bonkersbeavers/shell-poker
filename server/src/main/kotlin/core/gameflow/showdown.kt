6205004dc995f9f4e7ae526d961d50c4d062812d
Mateusz Szpyrka <mjsz1701@gmail.com>
2019-08-25 14:22:55 +0200

    Add better comments to resolveShowdown method

--- server/src/main/kotlin/core/gameflow/showdown.kt
+++ server/src/main/kotlin/core/gameflow/showdown.kt
@@ -12,16 +12,18 @@ fun resolveShowdown(handState: HandState): List<ShowdownAction> {
         else -> handState.orderedPlayers(handState.lastAggressor.position)
     }.filter { it.isInGame }
 
-    // when only one player is left in the game
+    // Player can muck his cards if he is the only one left in the hand.
     if (orderedPlayers.size == 1)
         return listOf(MuckCards(orderedPlayers.first().id))
 
-    // all players must show their cards if there are more streets to be dealt
+    // All players must show their cards if there are more streets to be dealt.
+    // This is the case when players go all in before the river and no further betting action is possible.
     if (handState.bettingRound != BettingRound.RIVER)
         return orderedPlayers.map { ShowCards(it.id) }
 
-    // one player definitely wins with another if he has a better hand
-    // and he has put at least the same amount of chips into the pot
+    // One player 'definitely wins' with another if he has a better hand and he has put
+    // at least the same amount of chips into the pot. If one player has a better hand than the other one
+    // but his chips commitment is lower, then the other player may still win some chips from side pots.
     fun definitelyWins(player: Player, other: Player): Boolean {
         val communityCards = handState.communityCards
         return (player.hand(communityCards) > other.hand(communityCards)) and (player.chipsInPot >= other.chipsInPot)
