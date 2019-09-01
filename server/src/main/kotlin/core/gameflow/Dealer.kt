f74c604c9a221bdd8e93dae416e1723f9b61bcc4
Mateusz Szpyrka <mjsz1701@gmail.com>
2019-09-01 10:00:37 +0200

    Refactor Dealer class to use HandState:rebuild method

--- server/src/main/kotlin/core/gameflow/Dealer.kt
+++ server/src/main/kotlin/core/gameflow/Dealer.kt
@@ -3,6 +3,7 @@ package core.gameflow
 import core.cards.Card
 import core.cards.baseDeck
 import core.gameflow.handstate.HandState
+import core.gameflow.handstate.rebuild
 import kotlin.random.Random
 
 class Dealer {
@@ -14,7 +15,7 @@ class Dealer {
         assert(handState.bettingRound == BettingRound.PRE_FLOP)
 
         val updatedPlayers = handState.players.map { it.withCards(listOf(deckIterator!!.next(), deckIterator!!.next())) }
-        return handState.copy(players = updatedPlayers)
+        return handState.rebuild(players = updatedPlayers)
     }
 
     fun dealFlop(handState: HandState): HandState {
@@ -23,7 +24,7 @@ class Dealer {
         assert(handState.bettingRound == BettingRound.FLOP)
 
         val flopCards = listOf(deckIterator!!.next(), deckIterator!!.next(), deckIterator!!.next())
-        return handState.copy(communityCards = flopCards)
+        return handState.rebuild(communityCards = flopCards)
     }
 
     fun dealTurn(handState: HandState): HandState {
@@ -32,7 +33,7 @@ class Dealer {
         assert(handState.bettingRound == BettingRound.TURN)
 
         val turnCard = deckIterator!!.next()
-        return handState.copy(communityCards = handState.communityCards + turnCard)
+        return handState.rebuild(communityCards = handState.communityCards + turnCard)
     }
 
     fun dealRiver(handState: HandState): HandState {
@@ -41,7 +42,7 @@ class Dealer {
         assert(handState.bettingRound == BettingRound.RIVER)
 
         val riverCard = deckIterator!!.next()
-        return handState.copy(communityCards = handState.communityCards + riverCard)
+        return handState.rebuild(communityCards = handState.communityCards + riverCard)
     }
 
     fun shuffle(seed: Int? = null) {
