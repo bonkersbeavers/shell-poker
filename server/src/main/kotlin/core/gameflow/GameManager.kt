5c02cb382abf38e881e2a047a9769fc5c0ac3fbf
Patryk Wójtowicz <patryk.zbigniew.wojtowicz@gmail.com>
2019-09-04 10:50:54 +0200

    Add first bugged implementation of GameManager

--- server/src/main/kotlin/core/gameflow/GameManager.kt
+++ server/src/main/kotlin/core/gameflow/GameManager.kt
@@ -1,4 +1,32 @@
 package core.gameflow
 
-class GameManager {
+import core.bettinground.ActionValidation
+import core.bettinground.BettingAction
+import core.bettinground.Fold
+import core.bettinground.ValidAction
+
+class GameManager(handState: HandState) {
+
+    val dealer: Dealer = Dealer()
+
+
+    fun bettingRound(handState: HandState) {
+
+        val activePlayer = handState.activePlayer!!
+
+        while(activePlayer != handState.lastAggressor) {
+            var action: BettingAction
+
+            do {
+                action = getAction(activePlayer)
+                val actionValidation: ActionValidation = action.validate(handState)
+            } while (actionValidation == ValidAction())
+        }
+    }
+
+    private fun getAction(player: Player): BettingAction {
+        return Fold();
+    }
+
+    private fun sendValidation(player: Player) {}
 }
