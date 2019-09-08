b75b2395d21ca8079325067225c2cbc257e38a0c
Patryk Wójtowicz <patryk.zbigniew.wojtowicz@gmail.com>
2019-09-08 10:46:18 +0200

    Add stub methods to PlayerRouter

--- server/src/main/kotlin/core/adapters/PlayerRouter.kt
+++ server/src/main/kotlin/core/adapters/PlayerRouter.kt
@@ -1,4 +1,14 @@
 package core.adapters
 
+import core.bettinground.ActionValidation
+import core.bettinground.BettingAction
+import core.bettinground.Fold
+import core.gameflow.handstate.HandState
+
 class PlayerRouter {
+    private fun getAction(playerId: Int): BettingAction = Fold
+    private fun update(playerId: Int, actionValidation: ActionValidation) {}
+    private fun update(playerId: Int, handState: HandState) {}
+    private fun broadcast(playerId: Int, action: BettingAction) {}
+    private fun broadcast(handState: HandState) {}
 }
