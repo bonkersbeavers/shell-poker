5c5919d69868df548e370550c9439330cdf26d0a
Patryk Wójtowicz <patryk.zbigniew.wojtowicz@gmail.com>
2019-09-09 20:48:57 +0200

    Add moveToBet method to Player

--- server/src/main/kotlin/core/gameflow/player/Player.kt
+++ server/src/main/kotlin/core/gameflow/player/Player.kt
@@ -39,6 +39,9 @@ data class Player(
         )
     }
 
+    //TODO: need tests???? XD
+    fun moveBetToPot(): Player = this.copy(chipsInPot = this.chipsInPot + this.bet, bet = 0)
+
     fun withCards(newCards: List<Card>): Player = this.copy(holeCards = newCards)
 
     /* Creates the best possible hand out of player's hole cards and community cards. */
