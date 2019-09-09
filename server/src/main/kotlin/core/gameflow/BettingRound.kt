9fe072302dfd28bee022c36fb6c308411b9b24b6
Patryk Wójtowicz <patryk.zbigniew.wojtowicz@gmail.com>
2019-09-09 20:45:45 +0200

    Add roundNumber to BettingRound enum

--- server/src/main/kotlin/core/gameflow/BettingRound.kt
+++ server/src/main/kotlin/core/gameflow/BettingRound.kt
@@ -1,8 +1,8 @@
 package core.gameflow
 
-enum class BettingRound {
-    PRE_FLOP,
-    FLOP,
-    TURN,
-    RIVER
+enum class BettingRound(val roundNumber: Int) {
+    PRE_FLOP(0),
+    FLOP(1),
+    TURN(2),
+    RIVER(3)
 }
