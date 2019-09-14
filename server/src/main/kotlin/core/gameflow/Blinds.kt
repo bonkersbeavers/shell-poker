f128701439eb70e81df2bcb9ece0041ef9d48806
Mateusz Szpyrka <mjsz1701@gmail.com>
2019-09-14 14:40:31 +0200

    Delete Post as BettingAction subclass, add postBlindsAndAnte function

--- server/src/main/kotlin/core/gameflow/Blinds.kt
+++ server/src/main/kotlin/core/gameflow/Blinds.kt
@@ -1,16 +1,38 @@
 package core.gameflow
 
-import core.bettinground.Post
+import core.bettinground.ActionType
 import core.gameflow.handstate.HandState
 
-fun postBlinds(handState: HandState): HandState {
-    val smallBlindValue = handState.blinds.smallBlind
-    val bigBlindValue = handState.blinds.bigBlind
+fun postBlindsAndAnte(stateBuilder: HandState.ImmutableBuilder, newPlayersIds: Set<Int>): HandState.ImmutableBuilder {
+    val players = stateBuilder.players!!
+    val blinds = stateBuilder.blinds!!
+    val positions = stateBuilder.positions!!
 
-    val afterSmallBlindState = Post(smallBlindValue).apply(handState)
-    val afterBigBlindState = Post(bigBlindValue).apply(afterSmallBlindState)
+    val playersWithAnte = players.map { player ->
+        when {
+            player.stack < blinds.ante -> player.copy(chipsInPot = player.stack, stack = 0)
+            else -> player.copy(chipsInPot = blinds.ante, stack = player.stack - blinds.ante)
+        }
+    }
 
-    return afterBigBlindState
+    val playersWithAnteAndBlinds = playersWithAnte.map { player ->
+        val chipsToPost = when {
+            player.id in newPlayersIds -> blinds.bigBlind
+            player.position == positions.bigBlind -> blinds.bigBlind
+            player.position == positions.smallBlind -> blinds.smallBlind
+            else -> 0
+        }
+
+        val playerAfterPost = when {
+            player.stack < chipsToPost -> player.copy(bet = player.stack, stack = 0)
+            else -> player.copy(bet = chipsToPost, stack = player.stack - chipsToPost)
+        }
+
+        val lastAction = if (chipsToPost > 0) ActionType.POST else null
+        playerAfterPost.copy(lastAction = lastAction)
+    }
+
+    return stateBuilder.copy(players = playersWithAnteAndBlinds)
 }
 
 data class Blinds(val smallBlind: Int, val bigBlind: Int, val ante: Int = 0)
